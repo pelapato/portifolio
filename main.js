71ce0306da25d189a102e445ea3ffa536eb14b2f
atilonpereira337@gmail.com <atilonpereira337@gmail.com>
2023-10-19 14:25:43 -0300

    atualiza;ao

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let loading=document.getElementById("loading")
 let contLogo=0
 let dialogo=document.getElementById("dialogo")
 let x=document.getElementById("x")
+
+let link1=document.getElementById("link1")
+let link2=document.getElementById("link2")
+let ceta=document.getElementById("ceta")
+function cetaClick(){
+  console.log("ola")
+}
+link1.addEventListener("click",cetaClick)
 const logotipo=()=>{
   contLogo+=1
   if(contLogo==1){
